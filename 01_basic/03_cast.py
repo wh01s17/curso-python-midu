5167c9093722c17f1f853462505706a9b48bdfc4
wh01s17 <jor.perezo@hotmail.com>
2025-04-04 16:02:41 -0300

    casting types

---
+++ 01_basic/03_cast.py
@@ -0,0 +1,22 @@
+###
+# 03 - casting de types
+# Transformar un tipo de un valor a otro
+###
+
+print("Conversion de tipos")
+# print(type( int("100") ))
+# print(int("100") + 2)
+# print("100" + str(2))
+
+# print(type( float("3.1416") ))
+# print( int(3.1416) )
+
+# print(bool(3))
+# print(bool(0))
+# print(bool(-1))
+
+# print(bool(""))
+# print(bool(" "))
+# print(bool("False"))
+
+
