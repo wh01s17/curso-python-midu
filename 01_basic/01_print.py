d652af20d321a06633a8e9f1a7a10fb18e085b50
wh01s17 <jor.perezo@hotmail.com>
2025-04-04 15:30:27 -0300

    initial commit

---
+++ 01_basic/01_print.py
@@ -0,0 +1,13 @@
+print("Hola, mundo!")
+
+print('Esto también funciona con comillas simples')
+
+print("Python", "es", "genial")
+
+print("Python", "es", "brutal", sep = "-")
+
+# por defecto, end renderiza un salto de linea, end = "\n'"
+print("Esto se imprime", end=" ")
+print("en una linea")
+
+print(42)
---
+++ 01_basic/02_types.py
@@ -0,0 +1,37 @@
+# tambien puede ser con comilla simple
+"""
+    Asi se puede hacer una cadena de texto multilinea, 
+    pero si no se asigna a nada,
+    se trata como comentario
+"""
+
+print("int")
+# print(10)
+# print(0)
+# print(-5)
+# print(type(123412341234123412341243123412))
+
+print("float")
+# print(type(3.14))
+# print(type(1.0))
+# print(type(1e3))
+
+print("complex")
+# print(type(1 + 2j))
+
+print("str")
+# print(type("Hola"))
+# print(type(""))
+# print(type("123"))
+# print(type("""
+#     Multilinea
+# """))
+
+print("bool")
+# print(type(True))
+# print(type(False))
+# print(type(1 < 2))
+
+print("NoneType")
+# print(type(None))
+
